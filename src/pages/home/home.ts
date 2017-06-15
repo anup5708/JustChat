e91aee13c2f795478787c323263a3a26f8445a1e
Anup Vishwakarma <ilanup371@gmail.com>
2017-06-15 14:47:09 +0530

    small changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SuperTabsController } from 'ionic2-super-tabs';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
